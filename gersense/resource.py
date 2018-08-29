bcdb72c8fa4855acae0fbbfba584c4255ecd3cf1
tiansi dong <tian1shi2@gmail.com>
2018-08-29 14:57:14 +0200

    add resource.py file add task.txt file

---
+++ gersense/resource.py
@@ -0,0 +1,4 @@
+word2vecDic={
+    "de": [("/home/kdml21/dongt/data/word2vec/de/de.tsv","https://github.com/Kyubyong/wordvectors"),
+           ("", "https://github.com/devmount/GermanWordEmbeddings")]
+}
